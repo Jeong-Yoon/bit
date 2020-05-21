905d6f415103e2e08abfb426b783eefb6050749c
kooeunhyeon <dmsgus0429@naver.com>
2020-05-21 11:22:10 +0900

    index.java 은현 수정 한번더

--- src/com/bit/exam/index.java
+++ src/com/bit/exam/index.java
@@ -4,5 +4,7 @@ public class index {
 	public static void main(String[] args) {
 		System.out.println("123");
 		System.out.println("충돌충도루~~~~~");
+		System.out.println("충돌충도루 한번더~~~~~!!!");
+		
 	}
 }
